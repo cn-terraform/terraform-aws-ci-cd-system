a4d3d5456df1feb5378ef9f45eaa448be7534ccb
Julian Nonino <noninojulian@gmail.com>
2020-05-27 10:11:53 +0100

    Updating module dependencies

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "networking" {
 #------------------------------------------------------------------------------
 module "jenkins" {
   source  = "cn-terraform/jenkins/aws"
-  version = "2.0.6"
+  version = "2.0.7"
   # source = "../terraform-aws-jenkins"
 
   name_preffix        = var.name_preffix
@@ -33,7 +33,7 @@ module "jenkins" {
 #------------------------------------------------------------------------------
 module "sonar" {
   source  = "cn-terraform/sonarqube/aws"
-  version = "2.0.10"
+  version = "2.0.11"
   # source = "../terraform-aws-sonarqube"
 
   name_preffix        = var.name_preffix
@@ -49,7 +49,7 @@ module "sonar" {
 #------------------------------------------------------------------------------
 module "nexus" {
   source  = "cn-terraform/nexus/aws"
-  version = "1.0.3"
+  version = "1.0.4"
   # source = "../terraform-aws-nexus"
 
   name_preffix        = var.name_preffix
