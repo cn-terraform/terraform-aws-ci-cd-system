4897a84680b0b4aea2dad0543fdcdf01568a745f
Julian Nonino <noninojulian@gmail.com>
2020-09-04 13:15:12 +0100

    Applying changes from terraform fmt command

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -6,8 +6,8 @@ module "cicd" {
   source                                      = "../../"
   name_preffix                                = "test-cicd"
   region                                      = "us-east-1"
-  availability_zones                          = [ "us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d" ]
+  availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
   vpc_cidr_block                              = "192.168.0.0/16"
-  public_subnets_cidrs_per_availability_zone  = [ "192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19" ]
-  private_subnets_cidrs_per_availability_zone = [ "192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19" ]
+  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
+  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
 }
