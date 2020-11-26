bb3fcdacf29356e9587946566d6d1d124752d925
Julian Nonino <noninojulian@gmail.com>
2020-11-26 15:04:19 +0000

    Updating networking module dependency

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 module "networking" {
   source  = "cn-terraform/networking/aws"
-  version = "2.0.10"
+  version = "2.0.12"
   #source = "../terraform-aws-networking"
 
   name_prefix                                 = var.name_prefix
