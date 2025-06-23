84c5bcbdf86ba209020e8742234223367911720b
Julian Nonino <jnonino@users.noreply.github.com>
2025-06-23 15:55:51 +0100

    Updating networking module

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -1,9 +1,44 @@
 module "cicd" {
-  source                                      = "../../"
-  name_prefix                                 = "test-cicd"
-  region                                      = "us-east-1"
-  availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
-  vpc_cidr_block                              = "192.168.0.0/16"
-  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
-  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+  source         = "../../"
+  name_prefix    = "test-cicd"
+  region         = "us-east-1"
+  vpc_cidr_block = "192.168.0.0/16"
+  
+  public_subnets = {
+    first_public_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.0.0/19"
+    }
+    second_public_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.32.0/19"
+    }
+    third_public_subnet = {
+      availability_zone = "us-east-1c"
+      cidr_block        = "192.168.64.0/19"
+    }
+    fourth_public_subnet = {
+      availability_zone = "us-east-1d"
+      cidr_block        = "192.168.96.0/19"
+    }
+  }
+
+  private_subnets = {
+    first_private_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.128.0/19"
+    }
+    second_private_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.160.0/19"
+    }
+    third_private_subnet = {
+      availability_zone = "us-east-1c"
+      cidr_block        = "192.168.192.0/19"
+    }
+    fourth_private_subnet = {
+      availability_zone = "us-east-1d"
+      cidr_block        = "192.168.224.0/19"
+    }
+  }
 }
